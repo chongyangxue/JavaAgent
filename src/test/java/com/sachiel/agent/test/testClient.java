22b00a99988c2c85fe41d75f58b28ffe2f924917
root <root@sachiel.(none)>
2014-08-01 14:41:04 -0400

    update

--- src/test/java/com/sachiel/agent/test/testClient.java
+++ src/test/java/com/sachiel/agent/test/testClient.java
@@ -15,8 +15,8 @@ import com.sachiel.agent.ClientUtils;
 public class testClient {
 	@Test
 	public void testAgentUtils() {
-		JSONObject result = ClientUtils.sendCommand("df -h", "http://10.11.6.59");
-		//JSONObject result = ClientUtils.sendCommand("ls /opt", "http://127.0.0.1");
+		JSONObject result = ClientUtils.sendCommand("ls -l /root | grep ^d | wc -l", "http://127.0.0.1");
+		//JSONObject result = ClientUtils.sendCommand("ls -l /log/sce3/999962587/20140701|grep ^- |wc -l", "http://10.121.41.119");
 		System.out.println(result.toString());
 	}
 }
